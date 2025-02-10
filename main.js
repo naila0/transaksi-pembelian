11a7594b5d19171cf6470ef2aa99058d79c2ea1d
naila0 <nailaf677@gmail.com>
2025-02-10 09:17:22 +0700

    WP menambah barang ke keranjang

---
+++ main.js
@@ -0,0 +1,76 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
+
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgSS-chZUH5T47nhRNeK6jYDnGZK_TQSA",
+  authDomain: "insan-cemerlang-d6eb1.firebaseapp.com",
+  projectId: "insan-cemerlang-d6eb1",
+  storageBucket: "insan-cemerlang-d6eb1.appspot.com",
+  messagingSenderId: "162904381844",
+  appId: "1:162904381844:web:dd88782fdcc494c9ac1781",
+  measurementId: "G-1RSX6TCWZ2"
+};
+
+//inisialisasi firebase
+const aplikasi = initializeApp(firebaseConfig)
+const basisdata = getFirestore(aplikasi)
+
+//fungsi ambil daftar barang 
+export async function ambilDaftarBarang() {
+  const refDokumen = collection(basisdata, "inventory");
+  const kueri = query(refDokumen, orderBy("item"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasilKueri = [];
+  cuplikanKueri.forEach((dokumen) => {
+    hasilKueri.push({
+      id: dokumen.id,
+      item: dokumen.data().item,
+      jumlah: dokumen.data().jumlah,
+      harga: dokumen.data().harga
+    })
+  })
+
+  return hasilKueri;
+}
+
+//Menambah barang ke keranjang 
+export async function tambahBarangKeKeranjang(
+  idbarang,
+  nama,
+  harga,
+  jumlah,
+  idpelanggan,
+  namapelanggan
+) {
+  try {
+    // Menyimpan data ke collection transaksi 
+    const refDokumen = await addDoc(collection(basisdata, "transaksi"), {
+      idbarang: idbarang,
+      nama: nama,
+      harga: harga,
+      jumlah: jumlah,
+      idpelanggan: idpelanggan,
+      namapelanggan: namapelanggan
+      })
+   
+  
+    // Menampilkan pesan berhasil
+    console.log("Berhasil menyimpan keranjang")
+  } catch (error) {
+    // Menampilkan pesan error
+    console.log(error)
+  }
+}
